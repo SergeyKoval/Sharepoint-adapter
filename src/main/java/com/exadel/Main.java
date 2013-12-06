873420d01526de294e6859975f01134b6b45bd50
PMitrafanau <pavel.109.fromBy@gmail.com>
2013-12-06 08:57:07 +0300

    Cleaned master branch

--- src/main/java/com/exadel/Main.java
+++ src/main/java/com/exadel/Main.java
@@ -1,32 +1,5 @@
 package com.exadel;
 
-import com.exadel.operations.Cleaner;
-import com.exadel.operations.Downloader;
-import com.exadel.operations.Uploader;
-import com.exadel.util.FBAHttpClientBuilder;
-import org.apache.http.client.HttpClient;
-
-import java.io.IOException;
-
 public class Main {
 
-    // users credentials from db (not active dir.!!!) for form base authentication
-    public static final String login = "Jack Sparrow";
-    public static final String password = "HG*as5W";
-
-    public static final String serverURL = "http://localhost:2004";
-
-
-    public static void main(String[] args) throws IOException {
-        FBAHttpClientBuilder httpClientBuilder = FBAHttpClientBuilder.getInstance();
-        HttpClient httpClient = httpClientBuilder.build(serverURL, login, password);
-
-        Uploader uloader = new Uploader(httpClient);
-        Downloader dloader = new Downloader(httpClient);
-        Cleaner cleaner = new Cleaner(httpClient);
-
-        uloader.upload(serverURL + "/library/FBA.mp4", "D:/FBA.mp4");
-        dloader.download(serverURL + "/library/FBA.mp4", "D:/Del/FBA.mp4");
-        cleaner.delete(serverURL + "/library/FBA.mp4");
-    }
 }
