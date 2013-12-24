97dec96bd8222c6ab83171a020ff8a9c2ce2871f
PMitrafanau <pavel.109.fromBy@gmail.com>
2013-12-24 11:56:19 +0300

    Moved microsoft classes into library. Change spsite to sp2013. Added instruction for own jar. Added dependency for own jar. Removed unused dependencies.

--- src/main/java/com/exadel/Main.java
+++ src/main/java/com/exadel/Main.java
@@ -10,12 +10,13 @@ import java.util.List;
 
 public class Main {
 
-    private static String login = "eltegra\\pmitrafanau";
-    private static String password = "HG*as5E";
+    private static String login = "eltegra\\skoval";
+    private static String password = "ENTER YOUR PASSWORD HERE";
 
 
     public static void main(String[] args) throws Exception {
         Operations operations = Operations.getInstance(login, password);
+        operations.addDocumentLibrary("Test", "Description");
 
         String listName = "Test";
         List<String> columnNames = new ArrayList<String>(Arrays.asList("LinkFilename", "FileRef"));
@@ -23,11 +24,11 @@ public class Main {
         operations.displaySPList(listName, columnNames, rowLimit);
 
         operations.uploadSPFile("D:\\copy.wsdl", "http://sp2013:5108/Test/copy.wsdl");
-        operations.downloadSPFile("http://localhost:12192/Test/copy.wsdl", "D:/copy1.wsdl");
+        operations.downloadSPFile("http://sp2013:5108/Test/copy.wsdl", "D:/copy1.wsdl");
 
         HashMap<String, String> fields = new HashMap<String, String>();
         fields.put("ID", "1"); // It isn't used (We can put here any id)
-        fields.put("FileRef", "http://localhost:12192/Test/lists.wsdl");
+        fields.put("FileRef", "http://sp2013:5108/Test/copy.wsdl");
 
         operations.deleteListItem(listName, fields);
     }
